3537f5d93a4a9378eb2692293061e1b9ec44bb36
Rakib Uddin <r2ifat@gmail.com>
2021-03-29 16:08:42 +0600

    Updated SOcket

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const cors = require('cors');
-const  express  =  require('express');
+const express  =  require('express');
+const RedisClient = require('redis');
+const createAdapter = require('socket.io-redis');
+
+
 const  app  =  express();
 
 app.use(cors());
 
 const server = require('http').createServer(app);
+
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
@@ -12,8 +17,16 @@ const io = require('socket.io')(server, {
     }
 });
 
+const pubClient = new RedisClient({ host: 'localhost', port: 6379 });
+const subClient = pubClient.duplicate();
+
+io.adapter(createAdapter({ pubClient, subClient }));
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.join(`room${process.env.PORT}`);
+    socket.broadcast.emit('hello', 'to all clients except sender');
+    socket.to('room8080').emit('hello', "to all clients in 'room42' room except sender");
 });
 
 app.get('/', function(req, res) {
